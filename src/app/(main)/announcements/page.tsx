0ea7a2e65bf5169e6cf80f0fde3a5bb9d0bd0de1
dlxks <dlxks.sangangbayan@gmail.com>
2025-11-12 20:14:21 +0800

    feat: update package.json and package-lock.json to use Prisma version 6.19.0; add HomeAnnouncementsPage component

---
+++ src/app/(main)/announcements/page.tsx
@@ -0,0 +1,8 @@
+const HomeAnnouncementsPage = () => {
+  return (
+    <div className="mx-auto max-w-7xl p-4 lg:p-8">
+      <h1>Announcements in Home</h1>
+    </div>
+  );
+};
+export default HomeAnnouncementsPage;
